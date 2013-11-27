ebb9b01319239d804aa51343ed1c191c964c1009
Rushingfitness <teikametrics@teikametricss-MacBook-Air.local>
2013-11-27 05:52:24 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ROI::Application.config.secret_token = '8f97d16d7e42ab074c8b98cd148c102bfbe6333ac45aa205f7b97ffe7c979e0837ac7e44044b2b56459aeee4ac2c94464cb9d117c58409b34def8d907b089c23'
